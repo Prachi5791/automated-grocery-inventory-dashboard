0cc3235dc5a6546d5458573cda098282385174eb
Prachi5791 <patilmg2009@gmail.com>
2026-08-18 14:16:37 +0530

    Initial project setup with Spring Boot and Maven

---
+++ src/main/java/com/prachi/inventory/AutomatedGroceryInventoryDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.prachi.inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedGroceryInventoryDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedGroceryInventoryDashboardApplication.class, args);
+	}
+
+}
